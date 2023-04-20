6f859ff6136c1020a25c56e3881336bb6297efbd
Bisllly <cuongnguyen3900@gmail.com>
2023-04-20 23:37:40 +0700

    try to random another 2 different note in notes

--- src/main/java/org/example/RandomNotes.java
+++ src/main/java/org/example/RandomNotes.java
@@ -1,2 +1,17 @@
-package org.example;public class RandomNotes {
+package org.example;
+
+public class RandomNotes {
+    public static void main(String[] args) {
+        //random a note in notes
+        String[] notes = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
+        int index = (int) Math.round(Math.random() * (notes.length - 1));
+        System.out.println(notes[index]);
+        //random another 2 different note in notes
+        int index2 = (int) Math.round(Math.random() * (notes.length - 1));
+        if (index2 != index) {
+            System.out.println(notes[index2]);
+        }
+
+    }
+
 }
--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -2,19 +2,6 @@ package org.example;
 
 public class Triangle {
     public static void main(String[] args) {
-        //prints out a triangle with 9 rows
-        //row
-        int maxRows = 4;
-        for (int row = 1; row <= 4; row++) {
-            int add = row - 1;
-            int maxSlots = 4 + add;
-            for (int slots = 1; slots <= maxSlots; slots+=2) {
-                for (int i = 1; i <= slots; i+=1) {
-                    System.out.print("x");
-                }
-                System.out.println();
-            }
 
-        }
     }
 }
