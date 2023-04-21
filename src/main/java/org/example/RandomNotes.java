0ce77ae309881c48ee8bf14260b96a67efb80106
cuongnguyen-oceansmart <cuongnguyen3900@gmail.com>
2023-04-21 13:01:55 +0700

    just try to take it in

--- src/main/java/org/example/RandomNotes.java
+++ src/main/java/org/example/RandomNotes.java
@@ -1,5 +1,26 @@
 package org.example;
-
+import java.util.Arrays;
+import java.util.HashSet;
 public class RandomNotes {
-    //given an array of 12 musical notes, random 1
+
+    public static void main(String[] args) {
+        //random a note in notes
+        int[] notes = new int[10];
+        for(int i=0; i< notes.length;i++){
+            notes[i] = i;
+//            System.out.println(notes[i]);
+        }
+        System.out.println(Arrays.toString(notes));
+        addNotes(notes);
+    }
+
+    private static void addNotes(int[] notes) {
+        HashSet<Integer> myNotes = new HashSet<>();
+        while (myNotes.size() < 3) {
+            int index = (int) Math.round(Math.random() * (notes.length - 1));
+            myNotes.add(notes[index]);
+        }
+        System.out.println(myNotes);
+    }
 }
+
