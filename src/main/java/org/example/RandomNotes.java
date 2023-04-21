18743b995fbf34a2dbeb3fac4c6f1a8958a0821d
cuongnguyen-oceansmart <cuongnguyen3900@gmail.com>
2023-04-21 13:19:09 +0700

    I did it, three notes

--- src/main/java/org/example/RandomNotes.java
+++ src/main/java/org/example/RandomNotes.java
@@ -4,18 +4,12 @@ import java.util.HashSet;
 public class RandomNotes {
 
     public static void main(String[] args) {
-        //random a note in notes
-        int[] notes = new int[10];
-        for(int i=0; i< notes.length;i++){
-            notes[i] = i;
-//            System.out.println(notes[i]);
-        }
-        System.out.println(Arrays.toString(notes));
+        String[] notes = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
         addNotes(notes);
     }
 
-    private static void addNotes(int[] notes) {
-        HashSet<Integer> myNotes = new HashSet<>();
+    private static void addNotes(String[] notes) {
+        HashSet<String> myNotes = new HashSet<>();
         while (myNotes.size() < 3) {
             int index = (int) Math.round(Math.random() * (notes.length - 1));
             myNotes.add(notes[index]);
